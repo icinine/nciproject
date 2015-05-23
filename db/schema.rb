a6cff38850d2d1509f8c819e1bd35c529b925aec
Shane Carr <shane.carr@sitesignals.com>
2015-05-23 15:13:30 +0000

    contracts model added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150521204931) do
+ActiveRecord::Schema.define(version: 20150523144600) do
 
   create_table "contracts", force: true do |t|
     t.string   "desc"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 20150521204931) do
     t.datetime "validTo"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
   create_table "users", force: true do |t|
